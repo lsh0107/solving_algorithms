4bf8f6d17f5c6ae1b941c5471aba4d82815f2102
lsh0107 <101846036+lsh0107@users.noreply.github.com>
2023-12-21 01:41:43 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,8 +1,8 @@
 def solution(a, b):
-    days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     day = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
-    months = 0
+    days = 0
     for i in range(a-1):
-        months += days[i]
-    answer = day[((months+b)%7)-1]
+        days += months[i]
+    answer = day[((days+b)%7)-1]
     return answer
