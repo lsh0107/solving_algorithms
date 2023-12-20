735614dfcdeb603afea92f2a383f995d49be23d1
lsh0107 <101846036+lsh0107@users.noreply.github.com>
2023-12-21 01:14:46 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,23 @@
+def solution(cards1, cards2, goal):
+    answer = ''
+    words = []
+    idx = 0
+    while idx < len(goal):
+        if cards1 and goal[idx] == cards1[0]:
+            words.append(cards1.pop(0))
+            idx += 1
+        
+        elif cards2 and goal[idx] == cards2[0]:
+            words.append(cards2.pop(0))
+            idx += 1
+        
+        else:
+            if words == goal:
+                answer = 'Yes'
+                return answer
+            else:
+                answer = 'No'
+                return answer
+            
+    answer = 'Yes'
+    return answer
