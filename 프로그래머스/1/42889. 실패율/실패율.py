616326c075f5cf1ea7d48efc86e8b10eaec43f51
lsh0107 <101846036+lsh0107@users.noreply.github.com>
2023-12-23 02:29:20 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -3,21 +3,15 @@ def solution(N, stages):
     # 1 <= stages <= 200,000
     # stages안에 각각의 stage: 1 <= stage <= N + 1
     answer = []
-
+    challenge = len(stages)
     for i in range(1, N+1):
-        challenge, fail, failure = 0, 0, 0
-        for stage in stages:
-            if stage == i:
-                fail += 1
-                challenge += 1
-            elif stage > i:
-                challenge += 1
+        fail = stages.count(i) #현재 스테이지에 머문 플레이어
         if challenge == 0:
             failure = 0
-        else: 
+        else:
             failure = fail/challenge
         answer.append((i, failure))
-
+        challenge = challenge - fail
     answer = sorted(answer, key=lambda x: (-x[1], x[0]))
     answer = [order[0] for order in answer]
     return answer
