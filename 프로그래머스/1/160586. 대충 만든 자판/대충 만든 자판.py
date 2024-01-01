5ae94deae669b250f5ffb05d06f6e5411e26e2d5
lsh0107 <101846036+lsh0107@users.noreply.github.com>
2024-01-02 02:05:45 +0900

    [level 1] Title: 대충 만든 자판, Time: 55.33 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -0,0 +1,21 @@
+def solution(keymap, targets):
+    answer = []
+
+    for target in targets:
+        ans = []      
+        for t in target:
+            temp = []
+            for key in keymap:
+                if t in key:
+                    temp.append(key.find(t) + 1) 
+                
+
+            if len(temp) == 0:
+                ans.append(-1)
+            else:
+                ans.append(min(temp))
+        if -1 in ans:
+            answer.append(-1)
+        else:    
+            answer.append(sum(ans))
+    return answer
