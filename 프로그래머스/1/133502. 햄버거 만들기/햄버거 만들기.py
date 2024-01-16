00206231a4091c07e9ddcd87dc2356ac474da567
lsh0107 <101846036+lsh0107@users.noreply.github.com>
2024-01-17 01:36:26 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,21 @@
+def solution(ingredient):
+    answer = 0
+    length = len(ingredient)
+    idx = 0
+    order = []
+    while ingredient:
+        
+        if length < 4 or idx == len(ingredient):
+            return answer
+        
+        if idx + 3 <= len(ingredient) and \
+            ingredient[idx:idx+4] == [1,2,3,1]:
+            answer += 1
+            del ingredient[idx:idx+4]
+            idx = idx - 2
+            length = len(ingredient)
+
+        else:
+            idx += 1
+    
+    return answer
