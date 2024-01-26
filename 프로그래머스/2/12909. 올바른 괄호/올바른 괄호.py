24e9068eb1817809efd38fcada2620f1a7b27e05
SangHyeok Lee <101846036+lsh0107@users.noreply.github.com>
2024-01-27 02:37:11 +0900

    [level 2] Title: 올바른 괄호, Time: 8.67 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = True
+    stack = []
+    for i in s:
+        if i == '(':
+            stack.append(i)
+        
+        else:
+            if stack and stack[-1] == '(':
+                stack.pop()
+            else:
+                return False
+    if stack:
+        return False
+    
+    return True
