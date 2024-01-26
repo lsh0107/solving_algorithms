07e12610756238e775da9cdabe860f532f390612
SangHyeok Lee <101846036+lsh0107@users.noreply.github.com>
2024-01-27 03:00:02 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.52 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -0,0 +1,18 @@
+def solution(s):
+    answer = [0, 0]
+
+    while len(s) > 1:
+        new_s = ''
+        count = s.count('0')
+        answer[0] += 1
+        answer[1] += count
+        s = s.replace('0', '')
+        length = len(s)
+        while length >= 1:
+            r = length % 2
+            length = length // 2
+            new_s += str(r)
+            
+        s = new_s
+    print(s)
+    return answer
