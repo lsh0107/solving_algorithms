f75789ea3a3785360cf3b845e48014476677ff4c
SangHyeok Lee <101846036+lsh0107@users.noreply.github.com>
2024-01-30 02:20:55 +0900

    [level 2] Title: 숫자의 표현, Time: 3.62 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -5,22 +5,17 @@ def solution(n):
     sum_num = 0
 
     while True:
-        if n in [1, 2] or num == n//2 + 1:
-            answer += 1
-            break
-
         num += 1
         sum_num += num
-        if sum_num == n:
-            t += 1
-            num = t
-            sum_num = 0
-            answer += 1
         
-        elif sum_num > n:
+        if sum_num >= n:
+            if sum_num == n:
+                answer += 1
             t += 1
             num = t
             sum_num = 0
         
+        if num > n:
+            break
 
     return answer
