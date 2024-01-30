58b5936a02a96c3c8059d6cb3a913afdde7c03d0
SangHyeok Lee <101846036+lsh0107@users.noreply.github.com>
2024-01-30 23:02:21 +0900

    [level 2] Title: 피보나치 수, Time: 570.47 ms, Memory: 439 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,8 +1,8 @@
 def solution(n):
     dp = [0, 1]
-    t = 0
-    while t < n:
+    t = 2
+    while t <= n:
         t += 1
         dp.append(dp[-2] + dp[-1])
         
-    return dp[-2]%1234567
+    return dp[-1]%1234567
