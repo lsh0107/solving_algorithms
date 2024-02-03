7cfb9b514d78f36453152d6e730ed38fe992a951
SangHyeok Lee <101846036+lsh0107@users.noreply.github.com>
2024-02-04 00:26:53 +0900

    [level 2] Title: 구명보트, Time: 15.13 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -2,16 +2,14 @@ def solution(people, limit):
     answer = 0
     min_ = 0
     max_ = 0
-    boat = []
     count = 0
     people = sorted(people)
     front = 0
     rear = -1
-    
+
     while True:
         min_ = people[front]
         max_ = people[rear]
-        boat.append(max_)
 
         if len(people) - count == 1:
             answer += 1
@@ -19,16 +17,14 @@ def solution(people, limit):
             rear -= 1
             break
 
-        if limit - boat[0] >= min_:
+        if limit - max_ >= min_:
             answer += 1
-            boat = []
             front += 1
             rear -= 1
             count += 2
         
         else:
             answer += 1
-            boat = []
             rear -= 1
             count += 1
 
