dc9d539e4b41e267778b3849c9e757983b8935e8
SangHyeok Lee <101846036+lsh0107@users.noreply.github.com>
2024-03-20 00:17:14 +0900

    [level 2] Title: 소수 찾기, Time: 0.41 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,25 @@
+def solution(numbers):
+    from itertools import permutations
+    answer = []
+    permutation = []
+    numbers = [num for num in numbers]
+    
+    for i in range(1, len(numbers)+1):
+        permutation += (list(permutations(numbers, i)))
+    
+    permutation = [int(('').join(per)) for per in permutation]
+    permutation = list(set(permutation))
+    
+    for num in permutation:
+        if num > 1 and is_prime(num):
+            answer.append(num)
+
+    return len(answer)
+
+
+def is_prime(num):
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            return False
+    return num
+        
