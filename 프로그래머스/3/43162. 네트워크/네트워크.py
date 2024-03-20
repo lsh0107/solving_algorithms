c669bb7bcc98999762d541bdd699a0ca8c9476ab
SangHyeok Lee <101846036+lsh0107@users.noreply.github.com>
2024-03-21 02:42:18 +0900

    [level 3] Title: 네트워크, Time: 0.22 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,18 @@
+def solution(n, computers):
+    answer = 0
+    
+    def dfs(idx, computers, visited):
+        if not visited[idx]:
+            visited[idx] = True
+            for next_ in range(len(computers[idx])):
+                if computers[idx][next_] == 1 and not visited[next_]:
+                    dfs(next_, computers, visited)
+
+    visited = [False] * (n)
+    
+    for idx in range(n):
+        if not visited[idx]:
+            dfs(idx, computers, visited)
+            answer += 1
+        
+    return answer
