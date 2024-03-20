49c4894b8394e14f119d1d0957b32fce0a6ec590
SangHyeok Lee <101846036+lsh0107@users.noreply.github.com>
2024-03-21 00:20:18 +0900

    [level 2] Title: 타겟 넘버, Time: 4.57 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,12 +1,15 @@
 def solution(numbers, target):
-    answer = dfs(0, 0, numbers, target)
-    return answer
-
-def dfs(index, curr_sum, numbers, target):
-    if index == len(numbers):
-        if curr_sum == target:
-            return 1
+    from collections import deque
+    answer = 0
+    queue = deque([(0,0)])
+    while queue:
+        index, curr_sum = queue.popleft()
+        if index == len(numbers):
+            if curr_sum == target:
+                answer += 1
+        
         else:
-            return 0
-    
-    return dfs(index + 1, curr_sum + numbers[index], numbers, target) + dfs(index + 1, curr_sum - numbers[index], numbers, target)
+            queue.append((index + 1, curr_sum + numbers[index]))        
+            queue.append((index + 1, curr_sum - numbers[index]))
+            
+    return answer
