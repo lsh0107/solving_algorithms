899c9a0d4f1bde681642f5a7bd9a82454646c28f
SangHyeok Lee <101846036+lsh0107@users.noreply.github.com>
2024-05-03 01:39:31 +0900

    [level 2] Title: 택배상자, Time: 283.97 ms, Memory: 94.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -0,0 +1,22 @@
+def solution(order):
+    answer = 0
+    con = [i for i in range(len(order), 0, -1)]
+    stack = []
+    i = 0
+    while True:
+        if con and order[i] == con[-1]:
+            con.pop()
+            i += 1
+            answer += 1
+
+        elif con and con[-1] < order[i]:
+            stack.append(con.pop())
+        
+        elif stack and stack[-1] == order[i]:
+            stack.pop()
+            answer += 1
+            i += 1
+
+        else:
+            if  (not con or not stack) or (order[i] != stack[-1] and order[i] != con[-1]):
+                return answer
