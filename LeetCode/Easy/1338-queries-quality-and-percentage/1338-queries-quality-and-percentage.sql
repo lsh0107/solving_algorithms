72928cbe130700c71040338c1eff536afa7f5c7e
SangHyeok Lee <101846036+lsh0107@users.noreply.github.com>
2024-06-03 20:45:28 +0900

    Delete LeetCode/Easy/1338-queries-quality-and-percentage directory

--- LeetCode/Easy/1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
+++
@@ -1,8 +0,0 @@
-# Write your MySQL query statement below
-SELECT 
-    query_name,
-    ROUND(SUM(rating/position)/COUNT(*), 2) as quality,
-    ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)/COUNT(*), 4)*100 as poor_query_percentage
-FROM Queries
-WHERE query_name IS NOT NULL
-GROUP BY query_name
