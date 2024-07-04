6dda213515b6346e8c1723b3df59c8166a077c53
SangHyeok Lee <101846036+lsh0107@users.noreply.github.com>
2024-07-05 02:21:09 +0900

    [Silver V] Title: 단어 정렬, Time: 96 ms, Memory: 36396 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -3,13 +3,11 @@ import sys
 input = sys.stdin.readline
 N = int(input())
 ans = []
-for _ in range(N):
-    word = input()[:-1]
-    if word in ans:
-        pass
-    else:
-        ans.append(word)
 
+for _ in range(N):
+    word = input().rstrip()
+    ans.append(word)
+ans = set(ans)
 ans = sorted(ans, key=lambda x: (len(x), x))
 
 for word in ans:
