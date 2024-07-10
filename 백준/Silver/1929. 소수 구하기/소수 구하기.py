37caad83f0562cf0f70d26db6a2bff0ccdef5130
SangHyeok Lee <101846036+lsh0107@users.noreply.github.com>
2024-07-10 17:15:11 +0900

    [Silver III] Title: 소수 구하기, Time: 5172 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,16 @@
+import sys
+
+def is_prime(x):
+    for i in range(2, int(x**0.5+1)):
+        if x % i == 0:
+            return False
+    return True
+
+input = sys.stdin.readline
+m, n = map(int, input().split())
+for i in range(m, n+1):
+    if i == 1:
+        continue
+    
+    if is_prime(i):
+        print(i)
