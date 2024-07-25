41f6a29dc7120c68bd6ae95fff65eea6b0531c51
SangHyeok Lee <101846036+lsh0107@users.noreply.github.com>
2024-07-25 19:25:03 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,24 +1,10 @@
 def solution(s):
-    answer = ''
-    chars = [s for s in s.split(' ')]
-    for i in range(len(chars)):
-        if chars[i] and chars[i][0].isdigit():
-            if i + 1 < len(chars):
-                answer += chars[i].lower() + ' '
-            else:
-                answer += chars[i].lower()
+    strings = list(s.split(' '))
+    ans = []
+    for i in range(len(strings)):
+        if strings[i]:
+            ans.append(strings[i][0].upper() + strings[i][1:].lower())
         else:
-            if chars[i]:
-                if i + 1 < len(chars):
-                    answer += chars[i][0].upper() + chars[i][1:].lower() + ' '
-                
-                else:
-                    answer += chars[i][0].upper() + chars[i][1:].lower()
-            
-            else:
-                if i + 1 == len(chars):
-                    return answer
-                else:
-                    answer += ' '
-
-    return answer
+            ans.append(strings[i])
+    
+    return ' '.join(ans)
