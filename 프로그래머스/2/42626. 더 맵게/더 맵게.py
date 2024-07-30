6f1225c64a14aa5a1cd66b7dbc78a9fb73c6968c
SangHyeok Lee <101846036+lsh0107@users.noreply.github.com>
2024-07-30 20:23:20 +0900

    [level 2] Title: 더 맵게, Time: 1594.06 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,18 +1,19 @@
 def solution(scoville, K):
     import heapq
-    answer = 0
+    ans = 0
     heapq.heapify(scoville)
     while True:
-        _min1 = heapq.heappop(scoville)
-        if _min1 >= K:
-            return answer
+        temp = 0
+        not_spicy1 = heapq.heappop(scoville)
+        if not_spicy1 < K:
+            not_spicy2 = heapq.heappop(scoville)
+            heapq.heappush(scoville, not_spicy1 + not_spicy2*2)
+            ans += 1
+        elif not_spicy1 >= K:
+            return ans
         
-        else:
-            _min2 = heapq.heappop(scoville)
-            heapq.heappush(scoville, _min1 + _min2*2)
-            answer += 1
-            
         if len(scoville) == 1 and scoville[0] < K:
-            return -1
+            break
         
-    return answer
+    return -1
+        
