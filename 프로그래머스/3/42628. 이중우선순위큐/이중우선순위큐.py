e5d2138747b3bd5b7ba982cfc0478d0ac67c0b13
SangHyeok Lee <101846036+lsh0107@users.noreply.github.com>
2024-07-31 22:00:44 +0900

    [level 3] Title: 이중우선순위큐, Time: 881.59 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,30 +1,32 @@
 def solution(operations):
     import heapq
-    answer = []
+    answer = [0, 0]
     max_heap, min_heap = [], []
     cnt = 0
-    for operation in operations:
-        if operation[0] == 'I':
-            heapq.heappush(max_heap, -int(operation[2:]))
-            heapq.heappush(min_heap, int(operation[2:]))
+    for op in operations:
+        order, num = op.split()
+        if order == 'I':
+            heapq.heappush(max_heap, -int(num))
+            heapq.heappush(min_heap, int(num))
             cnt += 1
-            
-        if cnt != 0 and operation[0] == 'D':
-            if max_heap and operation == 'D 1':
-                min_heap.remove(-heapq.heappop(max_heap))
-                heapq.heapify(min_heap)
-                cnt -= 1
-                
-            if min_heap and operation == 'D -1':
-                max_heap.remove(-heapq.heappop(min_heap))
+        
+        elif order == 'D':
+            if min_heap and int(num) == -1:
+                min_ = heapq.heappop(min_heap)
+                max_heap.remove(-min_)
                 heapq.heapify(max_heap)
                 cnt -= 1
-                
+            elif max_heap and int(num) == 1:
+                max_ = heapq.heappop(max_heap)
+                min_heap.remove(-max_)
+                heapq.heapify(min_heap)
+                cnt -= 1
+        
         if cnt == 0:
             max_heap, min_heap = [], []
-            
+    
     if cnt == 0:
-        return [0, 0]
-        
-    answer = [-max_heap[0], min_heap[0]]
+        return answer
+    
+    answer[0], answer[1] = -heapq.heappop((max_heap)), heapq.heappop(min_heap)
     return answer
