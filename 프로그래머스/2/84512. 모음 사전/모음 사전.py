ab6a3fac43e55710d606e44b4748ae98eac016ac
SangHyeok Lee <101846036+lsh0107@users.noreply.github.com>
2024-08-06 21:49:40 +0900

    [level 2] Title: 모음 사전, Time: 1.44 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,10 +1,8 @@
 def solution(word):
     from itertools import product
-    vowels = ['a','e','i','o','u']
-    vowels_list = []
-    for i in range(1, len(vowels)+1):
-        vowels_list += [''.join(permutation) for permutation in product(vowels, repeat=i)]
-    
-    vowels_list.sort()
-    answer = vowels_list.index(word.lower()) + 1
-    return answer
+    answer = []
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    for i in range(1, 6):
+        answer += [''.join(perm) for perm in product(vowels, repeat=i)]
+    answer.sort()
+    return answer.index(word.lower())+1
